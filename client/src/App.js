9ccb164cf2787fabdac9150a440e148fac53be78
soka1998 <soukainaharifi18@gmail.com>
2024-03-07 15:44:31 +0000

    add chat page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './components/Home';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Join from './components/Join';
+import Chat from './components/Chat';
 
 function App() {
   return (
   <Router>
-      <Home/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/Join" element={<Join/>} />
+      <Route path="/Chat" element={<Chat/>} />
+      </Routes>
       </Router>
   );
 }
